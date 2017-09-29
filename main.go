6ba00711076bc19c1ac7c8a98f45dae628b22c4a
Anson5555555Lee <frank5555555li@gmail.com>
2017-09-29 20:11:51 +0800

    draft search package

--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	_ "github.com/Anson5555555Lee/dataFeed/matchers"
+	"github.com/Anson5555555Lee/dataFeed/search"
+	"log"
+	"os"
 )
 
+func init() {
+	log.SetOutput(os.Stdout)
+}
+
 func main() {
-	fmt.Println("Hello World!!!")
+	search.Run("president")
 }
